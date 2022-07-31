95f0eae09c1d5dcd14ebf4f06fda592a576f7808
Saneera Silva <saneera@eight25media.com>
2022-07-31 16:32:32 +0530

    connect mongo atlas

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${config.db.DB_UN}:${config.db.DB_PW}@cluster0.yr8jn.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
