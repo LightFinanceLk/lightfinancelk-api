b1fe553eb23077497060fdfc1128513b91b0a04d
Saneera Silva <saneera@eight25media.com>
2022-07-31 16:34:22 +0530

    fix CORS headers

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 mongoose
   .connect(
     `mongodb+srv://${config.db.DB_UN}:${config.db.DB_PW}@cluster0.yr8jn.mongodb.net/?retryWrites=true&w=majority`
